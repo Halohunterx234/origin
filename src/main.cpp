a7c4c005b9eb07da76a25e552f4f92239cc8f161
Halohunterx234 <ultrahalogt@gmail.com>
2024-10-13 02:17:27 +0800

    cmake stuff, build works

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,11 @@
-#include <SDL2/SDL.h>
 #include <iostream>
-#include <stdio.h>
+#include <SDL3/SDL.h>
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+// You shouldn't really use this statement, but it's fine for small programs
+using namespace std;
 
-int main(int argc, char* args[]){
-    // Main window
-    SDL_Window* window = NULL;
-
-    //Surface contained by the window
-    SDL_Surface* screenSurface = NULL;
-
-    //Initialize SDL
-    //On error
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
-        printf( "SDL could not initalize! SDL_Error: %s\n", SDL_GetError() );
-    }
-    else {
-        //Create a window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if ( window == NULL )
-        {
-            printf( "SDL could not initalize! SDL_Error: %s\n", SDL_GetError() );
-        }
-    }
+// You must include the command line parameters for your main function to be recognized by SDL
+int main(int argc, char** args) {
+    cout << "omg it works";
+	return 0;
 }
-
