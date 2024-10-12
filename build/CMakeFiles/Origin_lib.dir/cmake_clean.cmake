623aaeff785ecea2c1b7192bd2c6043001e6e9da
Halohunterx234 <ultrahalogt@gmail.com>
2024-10-13 02:16:46 +0800

    added SDL3 submodule

---
+++ build/CMakeFiles/Origin_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Origin_lib.dir/src/main.cpp.o"
+  "CMakeFiles/Origin_lib.dir/src/main.cpp.o.d"
+  "libOrigin_lib.a"
+  "libOrigin_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Origin_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Origin_lib.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libOrigin_lib.a"
+)
